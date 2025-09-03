7e09ba513db315d4130cfa3c247d9afd5896813b
“[Aakritee <[aakriteeparajuli100@gmail.com]>
2025-09-03 08:33:10 +0545

    second  commit

--- InetAddressTypes.java
+++ InetAddressTypes.java
@@ -5,13 +5,18 @@ public class InetAddressTypes {
     public static void main(String[] args) {
         try {
             // InetAddress address = InetAddress.getByName("0.0.0.0");
-            InetAddress address = InetAddress.getByName("localhost");
+            // InetAddress address = InetAddress.getByName("localhost");
 
             // isanylocaladdress check
             // System.out.println("is AnyLocalAddress():" + address.isAnyLocalAddress());
 
             // isanyloopbackaddress check
-            System.out.println("is LoopAddress():" + address.isLoopbackAddress());
+            // System.out.println("is LoopAddress():" + address.isLoopbackAddress());
+
+            // Link-localAddress check
+            // InetAddress address = InetAddress.getByName("FE80:0000:0000::0000");
+            InetAddress address = InetAddress.getByName("169.254.0.0");
+            System.out.println("is LinkLocalAddress():" + address.isLinkLocalAddress());
 
         } catch (UnknownHostException e) {
             e.printStackTrace();
@@ -26,3 +31,6 @@ public class InetAddressTypes {
 // interface
 // Loopback address means device connecting with itself -eg.localhostaddress
 // -isLoopbackAddess()
+// Link-local address is an IP address that is intended for use only within the
+// communicate with sensor devices on a local network
+// segment.Example:169.254.x.x
