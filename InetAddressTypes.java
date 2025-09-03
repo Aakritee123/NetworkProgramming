3ca226598767b0f845152eb48e16939211d41627
“[Aakritee <[aakriteeparajuli100@gmail.com]>
2025-09-03 21:44:57 +0545

    modified

--- InetAddressTypes.java
+++ InetAddressTypes.java
@@ -15,8 +15,51 @@ public class InetAddressTypes {
 
             // Link-localAddress check
             // InetAddress address = InetAddress.getByName("FE80:0000:0000::0000");
-            InetAddress address = InetAddress.getByName("169.254.0.0");
-            System.out.println("is LinkLocalAddress():" + address.isLinkLocalAddress());
+            // InetAddress address = InetAddress.getByName("169.254.0.0");
+            // System.out.println("is LinkLocalAddress():" + address.isLinkLocalAddress());
+
+            // SitelocalAddress check
+            // sitelocaladress ranges from 10.0.0.0 to 10.255.255.255 or 172.16.0.0 to
+            // 172.31.255.255 or 192.168.0.0 to 192.168.255.255
+            // 192.168.x.x
+            // InetAddress address = InetAddress.getByName("10.0.0.0");
+            // InetAddress address = InetAddress.getByName("192.168.0.0");
+
+            // System.out.println("is LinkLocalAddress():" + address.isSiteLocalAddress());
+
+            // isMulticastAddress() check - It ranges from 224.0.0.0 to 239.255.255.255
+            // InetAddress address = InetAddress.getByName("224.0.0.0");
+            // System.out.println("is LinkLocalAddress():" + address.isMulticastAddress());
+            // isMCGlobal() check -It ranges from 224.0.1.0 to 238.255.255.255
+            InetAddress address = InetAddress.getByName("224.0.1.0");
+            System.out.println(address.isMCGlobal());
+            System.out.println();
+
+            // is MCOrgLocal() check ranges from FF0E to FF1E
+            InetAddress orgLocal = InetAddress.getByName("FF08::1");
+            System.out.println("Address: " + orgLocal.getHostAddress());
+            System.out.println("isMCOrgLocal(): " + orgLocal.isMCOrgLocal());
+            System.out.println();
+
+            // is MulticastSiteLocal() check -IPv6 Site-Local multicast range: FF05::/16
+            InetAddress siteLocal = InetAddress.getByName("FF05::1");
+            System.out.println("Address: " + siteLocal.getHostAddress());
+            System.out.println("isMCSiteLocal(): " + siteLocal.isMCSiteLocal());
+            System.out.println();
+
+            // is MulticastLinkLocal() check
+            // IPv4 Link-Local multicast range: 224.0.0.0 – 224.0.0.255
+            // IPv6 Link-Local multicast range: FF02::/16
+            InetAddress linkLocal = InetAddress.getByName("FF02::1");
+            System.out.println("Address: " + linkLocal.getHostAddress());
+            System.out.println("isMCLinkLocal(): " + linkLocal.isMCLinkLocal());
+            System.out.println();
+
+            // is MulticastNodeLocal() check IPv6 Node-Local multicast range: FF01::/16
+            InetAddress nodeLocal = InetAddress.getByName("FF01::1");
+            System.out.println("Address: " + nodeLocal.getHostAddress());
+            System.out.println("isMCNodeLocal(): " + nodeLocal.isMCNodeLocal());
+            System.out.println();
 
         } catch (UnknownHostException e) {
             e.printStackTrace();
@@ -34,3 +77,4 @@ public class InetAddressTypes {
 // Link-local address is an IP address that is intended for use only within the
 // communicate with sensor devices on a local network
 // segment.Example:169.254.x.x
+// Site-Local address can be used within a private network
